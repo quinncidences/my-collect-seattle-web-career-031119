7afcd6a9847bf98af99651c894f9a5997aea37ef
Quinn Cox <qrc.inpa@gmail.com>
2019-03-02 01:21:20 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
   i = 0
   new_collection = []
   while i < collection.length
-    yield collection[i].upcase << new_collection
+    new_collection << yield collection[i].upcase
     i += 1
   end
   new_collection
